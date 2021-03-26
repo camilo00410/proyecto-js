7aa8e36a8c0dc4d7908ffcffab934ee069f0e8b8
Camilo loaiza <camiloandres00410@gmail.com>
2021-03-25 23:03:14 -0500

    Slider
    Slider / Galeria de imagenes

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.galeria').bxSlider({
+        auto: true,
+        autoControls: true,
+        stopAutoOnClick: true,
+        pager: true,
+        slideWidth: 1200
+      });
+})
