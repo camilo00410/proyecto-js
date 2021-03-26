7a85148d429e553a487be0747675aac4d23f86a6
Camilo loaiza <camiloandres00410@gmail.com>
2021-03-26 03:13:35 -0500

    Cambiador de temas del sitio web

--- js/main.js
+++ js/main.js
@@ -58,4 +58,15 @@ $(document).ready(function(){
    });
 
 
-})
+    //    SELECTOR DE TEMA
+    var theme= $("#theme")
+    $("#to-green").click(function(){
+        theme.attr("href", "css/green.css");
+    });
+    $("#to-red").click(function(){
+        theme.attr("href", "css/red.css");
+    });
+    $("#to-blue").click(function(){
+        theme.attr("href", "css/blue.css");
+    });
+});
