c1932a48f81315cb2edddea681ee9fdd06af03b9
Camilo loaiza <camiloandres00410@gmail.com>
2021-04-29 19:07:28 -0500

    reloj

--- js/main.js
+++ js/main.js
@@ -110,7 +110,17 @@ $(document).ready(function(){
         })
     }
 
+    // Acordeon
     if(window.location.href.indexOf('about') > -1){
         $("#acordeon").accordion();
     }
+
+    // Reloj
+    if(window.location.href.indexOf('reloj') > -1){
+        setInterval(function(){
+            var reloj = moment().format('h:mm:ss');
+        $('#reloj').html(reloj);
+        }, 1000);
+    }
+
 });
